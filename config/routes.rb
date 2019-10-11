90d5ad136edf0b275703225d11831702d607dec8
Felipe <f.lastra.gonzalez@gmail.com>
2019-10-11 10:23:59 -0300

    other commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'dashboards/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root to: "home#index"
+root to: "dashboards#index"
 
 end
